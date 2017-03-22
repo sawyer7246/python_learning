b3c12cf4d503771541ba793e396fb1b08130ebc1
NIck <1789199491@qq.com>
2017-03-22 23:05:56 +0800

    2

---
+++ ex2/ex2.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+#print "Hello World!"
+print "Hello again"
+print "I like #typing this."#是不会转义
+print "Yay!Printing."
+#print "I'd much rather you 'not'."
+print 'I "said" do #not touch this.尝试一下'
